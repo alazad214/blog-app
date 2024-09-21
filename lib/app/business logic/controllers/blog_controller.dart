52f9424ce2851091f7d71170850d09d8e49c25f1
al azad <alazad214@gmail.com>
2024-09-21 22:03:18 +0600

    blogs added

--- lib/app/business logic/controllers/blog_controller.dart
+++ lib/app/business logic/controllers/blog_controller.dart
@@ -2,18 +2,26 @@ import 'package:get/get.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class BlogController extends GetxController {
-  // Timestamp কে readable date string এ convert করার function
   String formatDate(Timestamp timestamp) {
-    DateTime dateTime = timestamp.toDate();  // Timestamp থেকে DateTime তৈরি হচ্ছে।
-    return '${dateTime.day} ${getMonthName(dateTime.month)}, ${dateTime.year}';  // DateTime থেকে day, month, year নিয়ে format করা হচ্ছে।
+    DateTime dateTime = timestamp.toDate();
+    return '${dateTime.day} ${getMonthName(dateTime.month)}, ${dateTime.year}';
   }
 
-  // Month এর number কে name এ convert করার function
   String getMonthName(int month) {
     const months = [
-      'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
-      'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'
+      'Jan',
+      'Feb',
+      'Mar',
+      'Apr',
+      'May',
+      'Jun',
+      'Jul',
+      'Aug',
+      'Sep',
+      'Oct',
+      'Nov',
+      'Dec'
     ];
-    return months[month - 1];  // Month number দিয়ে month এর নাম নেয়া হচ্ছে।
+    return months[month - 1];
   }
 }
